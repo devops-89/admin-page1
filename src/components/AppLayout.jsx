b225174f2b34d96fdc9ea57fe0c44dbe742dfff8
anshuman619 <mishraanshuman619@gmail.com>
2025-01-06 18:11:30 +0530

    change

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import Navbar from './Navbar.jsx'
-import Sidebar from './Sidebar.jsx'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Navbar from './Navbar.jsx';
+import Sidebar from './Sidebar.jsx';
+import { Outlet } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Grid from "@mui/material/Grid2";
 
 const AppLayout = () => {
   return (
-    <>
-        <Navbar/>
-        <Outlet/>
-        <Sidebar/>
-    </>
-  )
-}
+    <Box>
+     
+      <Navbar />
 
-export default AppLayout
+      
+      <Grid container spacing={2} sx={{marginTop:"60px"}}>
+      
+        <Grid item xs={3} md={4}>
+          <Sidebar />
+        </Grid>
+
+        
+        <Grid item xs={9} md={8}>
+          <Outlet />
+        </Grid>
+      </Grid>
+    </Box>
+
+
+      
+
+
+  );
+};
+
+export default AppLayout;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,32 +10,39 @@ import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import Avatar from "@mui/material/Avatar";
 import { styled } from "@mui/material/styles";
+import logoSrc from "../assets/logo.png";
 
 const Search = styled("div")(({ theme }) => ({
-  backgroundColor: theme.palette.action.hover,
+  backgroundColor: "rgba(255, 255, 255, 0.2)", 
   padding: "0 10px",
   borderRadius: theme.shape.borderRadius,
   display: "flex",
   alignItems: "center",
   marginRight: theme.spacing(2),
+  color: "#fff", 
+  "& .MuiInputBase-input": {
+    color: "#fff", 
+  },
 }));
 
 const Navbar = ({
-  logo = "MyLogo",
+  logo = logoSrc,
   searchPlaceholder = "Search...",
   mailCount = 0,
   notificationCount = 0,
   avatarSrc = "/static/images/avatar/1.jpg",
 }) => {
   return (
-    <AppBar position="static" sx={{ bgcolor: "primary.main" }}>
+    // <AppBar position="fixed" sx={{ bgcolor: "black",boxShadow:"none",top:0 }}>
+    <AppBar sx={{ bgcolor: "black",position:"fixed",boxShadow:"none",top:0 }}>
       <Toolbar>
       
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          {logo}
-        </Typography>
+      <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
+          <img src={logo} alt="Logo" style={{ height: 40, marginRight: 10 }} />
+          
+        </Box>
 
-       
+      
         <Box display="flex" alignItems="center">
           
           <Search>
@@ -49,14 +56,14 @@ const Navbar = ({
             </Badge>
           </IconButton>
 
-          
+         
           <IconButton color="inherit">
             <Badge badgeContent={notificationCount} color="error">
               <NotificationsIcon />
             </Badge>
           </IconButton>
 
-       
+     
           <IconButton color="inherit">
             <Avatar alt="User" src={avatarSrc} />
           </IconButton>
