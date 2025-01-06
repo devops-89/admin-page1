a25a0e001586a0aac2766bae9e1f472d3ca35528
Himanshu Sharma <himanshu312cool@gmail.com>
2025-01-06 15:43:25 +0530

    css variable

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,56 @@
-import { Container } from '@mui/material'
+import { TextField, Button, Box, Typography, Container } from '@mui/material';
 import React from 'react'
+import Logo from '../assets/logo.png'
 
 const Login = () => {
-    return (
-        <>
-            <Container maxWidth="lg" sx={{ 'border': '2px solid red' }}>
-                Login
-            </Container>
+    const handleLogin = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+    <Container maxWidth="xs" sx={{'border' : '2px solid red'}}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <img src={Logo} alt='page 1 travels'/>
+        <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, backgroundColor:'var(--orange-color)' }}
+          >
+            Login Now
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
         </>
     )
 }
