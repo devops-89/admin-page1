ce1998bb616cc1a23de40f23a09320471b57682d
JanviGarg21 <janvi.garg@digixito.com>
2025-10-29 18:11:28 +0530

    change in base url

--- src/api/serverConstant.js
+++ src/api/serverConstant.js
@@ -2,11 +2,11 @@
 // const authenticationBaseUrl="http://192.168.1.21:3000";
 // const packageBaseUrl="http://192.168.1.21:3005";
 
-const authenticationBaseUrl="https://dev.page1travels.com/auth";
-const packageBaseUrl="https://dev.page1travels.com/admin";
-const baseURL = "https://dev.page1travels.com";
-const comissionBaseUrl="https://dev.page1travels.com/admin"
-const hotlierUrl="https://dev.page1travels.com/admin";
+const authenticationBaseUrl="https://api.page1travels.com/auth";
+const packageBaseUrl="https://api.page1travels.com/admin";
+const baseURL = "https://api.page1travels.com";
+const comissionBaseUrl="https://api.page1travels.com/admin"
+const hotlierUrl="https://api.page1travels.com/admin";
 
  
 
