3719f3515e490f8a2a57b3ddb026d7479dcda515
JanviGarg21 <janvi.garg@digixito.com>
2025-11-06 13:02:57 +0530

    BaseUrl Chane

--- src/api/serverConstant.js
+++ src/api/serverConstant.js
@@ -2,20 +2,18 @@
 // const authenticationBaseUrl="http://192.168.1.21:3000";
 // const packageBaseUrl="http://192.168.1.21:3005";
 
-const authenticationBaseUrl="https://api.dev.page1travels.com/auth";
-const packageBaseUrl="https://api.dev.page1travels.com/admin";
+const authenticationBaseUrl = "https://api.dev.page1travels.com/auth";
+
+const packageBaseUrl = "https://api.dev.page1travels.com/admin";
 const baseURL = "https://api.dev.page1travels.com";
-const comissionBaseUrl="https://api.dev.page1travels.com/admin"
-const hotlierUrl="https://api.dev.page1travels.com/admin";
+const comissionBaseUrl = "https://api.dev.page1travels.com/admin";
+const hotlierUrl = "https://api.dev.page1travels.com/admin";
 
-
-
-
-export const APIURL={
-    authenticationUrl:`${authenticationBaseUrl}/api`,
-    packageUrl:`${packageBaseUrl}/api`,
-    extraDetailUrl : `${baseURL}/utility-services/api/`,
-    hotlierUrl:`${hotlierUrl}/api`,
-    comissionUrl:`${comissionBaseUrl}/api`,
-    customerUrl:`${hotlierUrl}/api`
-}
+export const APIURL = {
+  authenticationUrl: `${authenticationBaseUrl}/api`,
+  packageUrl: `${packageBaseUrl}/api`,
+  extraDetailUrl: `${baseURL}/utility-services/api/`,
+  hotlierUrl: `${hotlierUrl}/api`,
+  comissionUrl: `${comissionBaseUrl}/api`,
+  customerUrl: `${hotlierUrl}/api`,
+};
