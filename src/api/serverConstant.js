8ad5534fbec3ecbfbd1897becb621e36901e35be
JanviGarg21 <janvi.garg@digixito.com>
2025-11-06 13:38:47 +0530

    Minor change in url

--- src/api/serverConstant.js
+++ src/api/serverConstant.js
@@ -4,6 +4,7 @@
 
 const authenticationBaseUrl = "https://api.uat.page1travels.com/auth";
 
+
 const packageBaseUrl = "https://api.uat.page1travels.com/admin";
 const baseURL = "https://api.uat.page1travels.com";
 const comissionBaseUrl = "https://api.uat.page1travels.com/admin";
